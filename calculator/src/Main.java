9ffe50ce9f659a9f3bb446d4339ff66bd9acef36
hilal567 <33893219+hilal567@users.noreply.github.com>
2018-01-16 22:56:00 +0300

    Add files via upload

---
+++ calculator/src/Main.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+public class Main {
+	public static void main(String[] args){
+		Gui g = new Gui();
+		g.setSize(250, 400);
+		g.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		g.setVisible(true);
+		g.setResizable(false);
+		
+		
+		
+	}
+
+}
